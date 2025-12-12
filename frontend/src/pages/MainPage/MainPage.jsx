8ab26b33690563bb9ed7b693869927dc1eefebd0
Denis Sopko <denis.reliable@gmail.com>
2025-12-12 16:07:57 +0200

    remove imports components

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 import styles from './MainPage.module.scss';
-import { Input } from '../../components/Input/Input';
-import { Button } from '../../components/Button/Button';
 import { AuthForm } from './AuthForm/AuthForm';
 import { RegisterForm } from './RegisterForm/RegisterForm';
 import { SwitcherForm } from './SwitcherForm/SwitherForm';
