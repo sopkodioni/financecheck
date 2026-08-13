03fa73b0af3c18d017992e01f9181610480c54fc
Denis Sopko <denis.reliable@gmail.com>
2026-08-13 17:54:49 +0300

    refactor: console.log replaced by Logger

--- backend/src/PrismaModule/prisma.service.ts
+++ backend/src/PrismaModule/prisma.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PrismaPg  } from "@prisma/adapter-pg"
-import { Pool } from "pg";
 import { PrismaClient } from "src/generated/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
+    private readonly logger = new Logger(PrismaService.name);
+
     constructor(configService: ConfigService){
         const connectionString = configService.getOrThrow<string>("DATABASE_URL");
         const adapter = new PrismaPg({ connectionString });
@@ -15,15 +16,15 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
     async onModuleInit() {
         try{
             await this.$connect();
-            console.log('Connect to DB  | SUCCEFUL')
+            this.logger.log('Connect to DB  | SUCCEFUL')
         } catch(e){
-            console.log('Connect to DB  | ERROR ')
+            this.logger.log('Connect to DB  | ERROR')
             process.exit(1)
         }
     }
 
     async onModuleDestroy() {
         await this.$disconnect();
-
+        this.logger.log('Disconnect DB')
     }
 }
