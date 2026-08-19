a2f5b99c10bda9645257a6add1754d2468017aa1
Denis Sopko <denis.reliable@gmail.com>
2026-08-19 23:50:55 +0300

    wip: users module in progress

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import * as path from 'path'
 import { PrismaModule } from './PrismaModule/prisma.module'
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ 
     isGlobal: true,
     envFilePath: path.resolve(process.cwd(), '../.env')
-  }), PrismaModule],
+  }), PrismaModule, UsersModule],
   controllers: [],
   providers: [],
 })
---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,4 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {}
