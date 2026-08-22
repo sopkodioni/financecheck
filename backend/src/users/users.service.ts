a29c64833724b7d3cadd63c976ef87c4d7e5bd29
Denis Sopko <denis.reliable@gmail.com>
2026-08-22 21:08:10 +0300

    feature(users): implement users service methods

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from 'src/prisma/generated/browser';
+import { PrismaService } from 'src/PrismaModule/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) {}
+
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        return this.prisma.user.create({ data });
+    }
+
+    async updateProfile(id: string, data: Prisma.UserUpdateInput): Promise<User> {
+        return this.prisma.user.update({
+            where: { id }, 
+            data
+        });
+    }
+
+    async updateEmail(id: string, data: Prisma.UserUpdateInput): Promise<User> {
+        return this.prisma.user.update({
+            where: { id }, 
+            data
+        });
+    }
+
+    async updatePassword(id: string, data: Prisma.UserUpdateInput): Promise<User> {
+        return this.prisma.user.update({
+            where: { id }, 
+            data
+        });
+    }
+
+    async findById(id: string): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: { id }
+        });
+    }
+
+    async delete(id: string): Promise<User> {
+        return this.prisma.user.delete({ 
+            where: { id }
+         });
+    }
+}
