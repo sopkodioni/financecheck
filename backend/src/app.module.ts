6fc8cd240e78c1845f5aff9f948a18e8bd61f649
Denis Sopko <denis.reliable@gmail.com>
2026-08-30 23:57:20 +0300

    chore(auth): import AuthModule to AppModulee

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,18 @@ import { ConfigModule } from '@nestjs/config'
 import * as path from 'path'
 import { PrismaModule } from './PrismaModule/prisma.module'
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ 
-    isGlobal: true,
-    envFilePath: path.resolve(process.cwd(), '../.env')
-  }), PrismaModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+      envFilePath: path.resolve(process.cwd(), '../.env')
+    }), 
+    PrismaModule, 
+    UsersModule,
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
